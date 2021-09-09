f26478113a7dcfae4ca5d0635959d32128dbb5bd
Imran N <team@yieldmore.org>
2021-09-09 22:13:32 +0530

    basic mindmap (gitmind) and node extract / copy

---
+++ sections/unzip-all.bat
@@ -0,0 +1,1 @@
+node unzip-main.js
---
+++ sections/unzip-setup.bat
@@ -0,0 +1,7 @@
+rem install nodejs which includes its npm (node package manager)
+rem NOT NEEDED npm i -g global-npm
+call npm i -g unzipper
+call npm link unzipper
+call npm i -g fs-extra
+call npm link fs-extra
+pause
