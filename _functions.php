5d6514d22281df60b51c369ae1a14d06f0b8cd2b
Imran N <team@yieldmore.org>
2021-09-21 22:32:27 +0530

    section with subfolders... (folders / authors)

--- _functions.php
+++ _functions.php
@@ -7,10 +7,10 @@ function active_if($node) {
 
 cs_var('sections', [
 	['name' => 'Content',	'slug' => 'pages', 		'extensions' => 'txt'],
-	['name' => 'Articles',	'slug' => 'authors', 	'extensions' => 'txt', 'subfolder' => 'flat'],
+	['name' => 'Articles',	'slug' => 'authors', 	'extensions' => 'txt', 'subfolder' => true],
 	['name' => 'People',	'slug' => 'people', 	'extensions' => 'txt'],
 	['name' =>'Initiatives','slug' => 'initiatives','extensions' => 'txt'],
-	['name' => 'AM Drive',	'slug' => 'downloads',	'extensions' => 'pdf, mp3', 'content' => 'txt', 'prepend' => 'jpg', 'subfolder' => 'slug'],
+	['name' => 'Folders',	'slug' => 'downloads',	'extensions' => 'png, jpg, txt, pdf, mp3', 'content' => 'txt', 'prepend' => '', 'subfolder' => true],
 	['name' => 'Resources',	'slug' => 'data', 		'extensions' => 'tsv'],
 ]);
 
@@ -18,22 +18,48 @@ function before_render() {
 	if (cs_var('node') == 'go') { include_once 'resources.php'; exit; }
 	$section = false;
 	$file = false;
+	$fol = false;
 
 	foreach (cs_var('sections') as $s) {
 		$path = cs_var('path') . '/' . $s['slug'] . '/';
-		$file = $path . cs_var('node') . (isset($s['subfolder']) && $s['subfolder'] == 'slug' && cs_var('page_parameter1') ? '/' . cs_var('page_parameter1') : '') . '.';
-		foreach (explode(', ', $s['extensions']) as $extn) {
-			if (file_exists($file . $extn)) {
-				cs_var('file', $file = $file . $extn);
-				cs_var('extn', $extn);
+		if (isset($s['subfolder'])) {
+			$fol = $path . cs_var('node') . '/';
+			if (is_dir($fol)) {
+				cs_var('fol', $fol);
+				cs_var('folName', cs_var('node'));
 				$section = $s;
 				break;
+			} else if (is_dir($path)) {
+				foreach (scandir($path) as $i) {
+					if ($i == '.' || $i == '..') continue;
+					$d = $path . $i . '/';
+					if (is_section_file($s, $d . cs_var('node') . '.')) {
+						cs_var('fol', $d);
+						cs_var('folName', $i);
+						$section = $s;
+						break;
+					}
+				}
 			}
+		} else if (is_section_file($s, $path . cs_var('node') . '.')) {
+			$section = $s;
+			break;
 		}
+		
 		if ($section) break;
 	}
 
 	cs_var('section', $section);
+}
+
+function is_section_file($s, $file) {
+	foreach (explode(', ', $s['extensions']) as $extn) {
+		if (file_exists($file . $extn)) {
+			cs_var('file', $file = $file . $extn);
+			cs_var('extn', $extn);
+			return true;
+		}
+	}
 }
 
 function did_render_page() {
@@ -71,8 +97,20 @@ function section_banner($section) {
 
 function print_sections_menu() {
 	$nl = cs_var('nl');
-	echo '<div class="row menu">' . $nl;
+	echo $nl . $nl . '<div class="row menu">' . $nl;
 	$node = cs_var('node');
+	
+	if (cs_var('fol')) {
+		echo '	<div class="col-3">' . $nl;
+		echo '		<h2 class="selected">&hellip; ' . cs_var('folName') . '</h2>' . $nl;
+
+		$last_file = '';
+		foreach (scandir(cs_var('fol')) as $i) {
+			$fwe = print_section_file($nl, $node, $last_file, $i);
+			$last_file = $fwe;
+		}
+		echo '	</div>' . $nl;
+	}
 
 	foreach (cs_var('sections') as $s) {
 		echo '	<div class="col-3">' . $nl;
@@ -82,17 +120,23 @@ function print_sections_menu() {
 
 		$last_file = '';
 		foreach ($files as $file) {
-			if ($file == '.' || $file == '..') continue;
-			$file = explode('.', $file, 2)[0];
-			if ($last_file == $file) continue;
-			echo sprintf('		<a%s href="%s">%s</a>' . $nl,
-				($node == $file ? ' class="selected"' : ''), cs_var('url') . $file . '/', str_replace('-', ' ', $file));
-			$last_file = $file;
+			$fwe = print_section_file($nl, $node, $last_file, $file);
+			$last_file = $fwe;
 		}
-		
+
 		echo '	</div>' . $nl;
 	}
 
 	echo '</div>' . $nl;
 }
+
+function print_section_file($nl, $node, $last_file, $file) {
+	if ($file == '.' || $file == '..') return $last_file;
+	$file = explode('.', $file, 2)[0];
+	if ($last_file == $file) return $file;
+	echo sprintf('		<a%s href="%s">%s</a>' . $nl,
+		($node == $file ? ' class="selected"' : ''), cs_var('url') . $file . '/', str_replace('-', ' ', $file));
+	return $file;
+}
+
 ?>
