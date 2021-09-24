2e79d548d41a7109320038fa9d02ee9bb2ccfa7c
Imran N <team@yieldmore.org>
2021-09-25 00:35:35 +0530

    use excerpts in home page

--- index.php
+++ index.php
@@ -1,8 +1,25 @@
 <?php section_banner(); ?>
-<h1 class="heading">Welcome to YieldMore.org</h1>
+<div class="banner below"><?php echo markdown('&bull; a painting of [niggle](https://imran.yieldmore.org/niggle/) and [his mountains](https://legacy.yieldmore.org/niggle) by a friend Maria - <u title="Because it reinforces the needs for art in our world where we need badly to listen to our artists and their feelings their works evoke in us">why is it here?</u>'); ?></div>
 
-<?php
-//Soon we are to look <a href="https://themes.semicolonweb.com/html/canvas/demo-nonprofit.html" target="_blank">like this ^_^</a>.
-?>
-
-Understand us from <a class="hilight" href="matrix/">our matrix</a>,  <a class="hilight" href="sunlight/">evolving sunlight</a> or <a class="hilight" href="education/">education</a> and  <a class="hilight" href="children/">curation based education</a>.
+<div id="content">
+	<h1 class="heading">Welcome to YieldMore.org</h1>
+	
+	
+	
+	<h1>Imran's Welcome</h1>
+	<?php echo excerpt('/authors/imran/welcome.txt', 'welcome/', '&hellip; Read More'); ?>
+	
+	<h1>The YM Matrix</h1>
+	<?php echo excerpt('/downloads/about-us/matrix.txt', 'matrix/', '&hellip; Read More'); ?>
+	
+	<h1>Children</h1>
+	<?php echo excerpt('/pages/children.txt', 'children/', '&hellip; Read More asbout Children and their Emotional Development'); ?>
+	
+	<h1>Education</h1>
+	<?php echo excerpt('/pages/education.txt', 'education/', '&hellip; Read More'); ?>
+	
+	<h1>Evolving Sunlight</h1>
+	<?php echo excerpt('/initiatives/sunlight.txt', 'sunlight/', '&hellip; Read More'); ?>
+	
+	Understand us from <a class="hilight" href="matrix/">our matrix</a>,  <a class="hilight" href="sunlight/">evolving sunlight</a> or <a class="hilight" href="education/">education</a> and  <a class="hilight" href="children/">curation based education</a>.
+</div>
