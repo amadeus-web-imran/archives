331ce6dd864446b8d211643aff90d49b5c4a56a4
Imran N <team@yieldmore.org>
2022-01-13 06:55:58 +0530

    slight reorg (evolve), members, goodwill, serenity new affirmation

---
+++ scripts/members.php
@@ -0,0 +1,12 @@
+<?php
+$loggedIn = false;
+if (!$loggedIn) {
+echo markdown('# Consider joining us
+
+# Signup
+
+<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfouH5ae7RObekUO4JoEN8SF0wqFrkjzfIV-k-2sZhQtxXkew/viewform?embedded=true" width="640" height="986" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+');
+
+}
+?>
